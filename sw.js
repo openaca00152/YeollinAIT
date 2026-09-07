08c61c71780bae5fb05e547af854789136ae3108
openaca00152 <openaca00152@gmail.com>
2026-09-07 11:05:57 +0900

    개인 기기에서 이용 코드 기억하기 (#2)
    정상 이용 코드만 학생의 개인 기기에 선택적으로 저장하고, 즉시 삭제 기능과 모바일 안내를 추가합니다.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yeollin-ait-v2';
+const CACHE_NAME = 'yeollin-ait-v3';
 const APP_SHELL = ['./', './index.html', './config.js', './app.js', './styles.css', './manifest.json', './icon.png'];
 
 self.addEventListener('install', (event) => {
