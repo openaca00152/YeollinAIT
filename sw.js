25c2885b3f5126d4fe36703c1198e047e7edc256
openaca00152 <openaca00152@gmail.com>
2026-09-11 12:01:10 +0900

    Restore photo crop flow in sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yeollin-ait-v9';
+const CACHE_NAME = 'yeollin-ait-v10';
 const APP_SHELL = ['./', './index.html', './admin.html', './config.js', './app.js', './styles.css', './manifest.json', './icon.png'];
 
 self.addEventListener('install', (event) => {
