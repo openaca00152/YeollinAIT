d87b3be40be10bc8dc30ccecfb1665f89b142160
openaca00152 <openaca00152@gmail.com>
2026-09-11 12:07:30 +0900

    학생용 오류 안내 한글화
    * Show Korean errors in app.js
    
    * Show Korean errors in sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yeollin-ait-v10';
+const CACHE_NAME = 'yeollin-ait-v11';
 const APP_SHELL = ['./', './index.html', './admin.html', './config.js', './app.js', './styles.css', './manifest.json', './icon.png'];
 
 self.addEventListener('install', (event) => {
